9b1367c9e35c195c3562db10cf984c6a551ef101
Belinda Xi <belindax@fb.com>
2021-06-25 15:08:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,50 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+    // new
+    ImageView ivPoster;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        // new
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
+
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        String imageUrl = movie.getPosterPath();
+//        Glide
+//                .with(context)
+//                .load(imageUrl)
+//                .placeholder(R.drawable.flicks_movie_placeholder)
+//                .into(ivPoster);
+
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
+    }
+}
